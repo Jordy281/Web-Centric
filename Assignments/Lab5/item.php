5e213691f71c9f72dabc379bdb68745c8ad5995b
Jordan Stirling <Jordan.Stirling@dal.ca>
2015-12-07 14:42:38 -0400

    Update item.php
    created function to return items from previous car to array

--- Assignments/Lab5/item.php
+++ Assignments/Lab5/item.php
@@ -1,14 +1,60 @@
 <?php
     class Item{
         public $name;
-        //public $id;
+        public $id;
         public $value;
+        public $cartID;
         
         public function __construct($name, $value){
             $this->name=$name;
-        //    $id=$this->id;
             $this->value=$value;
         }
+    }
+    
+    public function insert($dbc,$cid){
+            
+        $query = "INSERT INTO basket(cartID,itemName,Value) VALUES(?,?,?)";
+        
+        $stmt=mysqli_prepare($dbc, $query);
+                
+        if ( !$stmt ) {
+            die('mysqli error: '.mysqli_error($dbc));
+        }
+                
+        mysqli_stmt_bind_param($stmt, "dsd", $cid,$this->name,$this->value);
+                
+        if ( !mysqli_execute($stmt) ) {
+            die( 'stmt error: '.mysqli_stmt_error($stmt) );
+        }
+        
+        $this->id=mysqli_stmt_insert_id($stmt);
+    }
+    
+    public function load($dbc,$cid){
+        $query = "SELECT * FROM basket WHERE cartID=?"
+        $stmt=mysqli_prepare($dbc,$query);
+        
+        if ( !$stmt ) {
+            die('mysqli error: '.mysqli_error($dbc));
+        }
+        
+        mysqli_stmt_bind_param($stmt,"d",$cid);
+        
+        if ( !mysqli_execute($stmt) ) {
+            die( 'stmt error: '.mysqli_stmt_error($stmt) );
+        }
+        
+        mysqli_stmt_bind_result($stmt, $id, $name,$value);
+        $items= array();
+        while (mysqli_stmt_fetch($stmt)) {
+            $item = new Item($name,$value);
+            array_push($items,$name);
+        }
+        //return array
+        return $items;
+    }
+
+        
     }
     
     
