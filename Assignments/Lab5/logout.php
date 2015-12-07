1262702a37d71fbf7467e23d1aa32457efc4626a
Jordan Stirling <Jordan.Stirling@dal.ca>
2015-12-07 18:04:13 -0400

    Update logout.php
    updated to save cart

--- Assignments/Lab5/logout.php
+++ Assignments/Lab5/logout.php
@@ -1,13 +1,14 @@
 <?php
-    
+    require_once('../mysqli_connect.php');
     //save cart
+    unserialize($_SESSION['cart'])->saveCart($dbc);
     
     //clear session data
     var_dump($_SESSION['user']);
     var_dump($_SESSION['cart']);
     
     //disconnect database
-    require_once('../mysqli_connect.php');
+    
     mysqli_close();
     
     //redirect to login
